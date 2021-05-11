b88201ccbf2a8482c41e629f79e505dc960ca376
Nicklas Eriksson <salk1n@hotmail.com>
2021-05-11 12:25:03 +0200

    Add project files.

---
+++ HouseholdEconomyG4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HouseholdEconomyG4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
